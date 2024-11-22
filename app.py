6437d6c32a67fc70319568698e36841aa496fbe1
Caroline Noyer <caroline@WorkMacCaro.local>
2024-11-22 13:58:14 +0100

    feat: 🎸 Logs
    Add log system

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from utilities import display
+import sys
+from dotenv import load_dotenv
+from utilities import display, logs
 
 display.clear_screen()
+
+# Load env variables
+try:
+    load_dotenv()
+except (ValueError, Exception):
+    sys.exit(display.alert("Configuration could not be loaded (" + repr(Exception) + ")"))
+
+# Initialize logs
+logs.init_file()
 
 if __name__ == '__main__':
     display.alert('Hello World')
